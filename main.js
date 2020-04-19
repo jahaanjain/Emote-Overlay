58301e78b7a42c0b65496d63af2258a217c3545a
Jahaan Jain <jahaanjain@gmail.com>
2020-04-19 18:41:46 -0400

    Added emote detection in messages

--- main.js
+++ main.js
@@ -20,12 +20,12 @@ let emotes = [];
 async function getEmotes(check) {
     const proxyurl = 'https://cors-anywhere.herokuapp.com/';
     // first, we need to get the channels ID
-    let res = await fetch(proxyurl + 'https://customapi.aidenwallis.co.uk/api/v1/twitch/toID/' + channel, { // using aidenwallis API for now to get twitch ID from username
+    let res = await fetch('https://api.ivr.fi/twitch/resolve/' + channel, {
         method: "GET"
-        }).then(function(response) { return response.text() },
+        }).then(function(response) { return response.json() },
         function(error) { log(error.message); }
     );
-    let channelID = res;
+    let channelID = res.id;
     log(channelID)
     // next, we want to get a list of every channel sub emote, if any.
     res = await fetch(proxyurl + 'https://api.twitchemotes.com/api/v4/channels/' + channelID, {
@@ -77,13 +77,42 @@ async function getEmotes(check) {
 }
 
 // Find Emotes and return name + link
-function findEmote(name) {
+function returnEmote(name) {
     if (emotes.length > 0) {
         // coming soon
     } else { log('emotes are still being processed'); }
 }
+let currentStreak = { streak: 0, emote: null }; // the current emote streak being used in chat
+let currentEmote; // the current emote being used in chat
+let showEmote; // the emote shown from using the !showemote <emote> command
+let minStreak = 5; // minimum emote streak to trigger overlay effects
 
-// Main module for connecting to twitch chat
+function findEmotes(message) {
+    if (emotes.length !== 0) {
+        message = message.split(' ');
+        if (message.includes(currentStreak.emote)) { currentStreak.streak++; }
+        else {
+            currentStreak.streak = 0;
+            currentStreak.emote = findEmoteInMessage(message);
+        }
+
+        function findEmoteInMessage(message) {
+            for (const emote of emotes.map(a => a.emoteName)) {
+                if (message.includes(emote)) { return emote; }
+            }
+            return null;
+        }
+        // add something to trigger streakEvent()
+    }
+}
+
+function streakEvent() {
+    if (currentStreak.streak >= minStreak) {
+        // find a way to trigger animations
+    }
+}
+
+// Connecting to twitch chat
 function connect() {
 
     const chat = new WebSocket("wss://irc-ws.chat.twitch.tv");
@@ -104,15 +133,17 @@ function connect() {
     chat.onerror = function() {
         log("There was an error.. disconnected from the IRC");
         chat.close();
+        chat.connect();
     };
 
     chat.onmessage = function(event) {
         let messageFull = event.data.split(/\r\n/)[0].split(`;`);
-        log(messageFull)
-        if (messageFull.length == 14) {
-            let messageBefore = messageFull[13].split(`${channel} :`).pop(); // gets the raw message
+        // log(messageFull)
+        if (messageFull.length > 12) {
+            let messageBefore = messageFull[messageFull.length - 1].split(`${channel} :`).pop(); // gets the raw message
             let message = (messageBefore.split(' ').includes('ACTION')) ? messageBefore.split('ACTION ').pop().split('')[0] : messageBefore; // checks for the /me ACTION usage and gets the specific message
-            log(message)
+            // log(message)
+            findEmotes(message)
         }
     };
 }
