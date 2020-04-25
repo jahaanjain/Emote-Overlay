50913625bbd37ee433107afb7537aab10177db6d
Jahaan Jain <jahaanjain@gmail.com>
2020-04-25 12:56:01 -0400

    Finished everything, might add animations later?

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let debug = true;
+let debug = false;
 function log(message) { if (debug) { console.log(message); } }
 
 // Get URL Parameters (Credit to html-online.com)
@@ -93,8 +93,7 @@ async function getEmotes(check) {
 let currentStreak = { streak: 1, emote: null, emoteURL: null }; // the current emote streak being used in chat
 let currentEmote; // the current emote being used in chat
 let showEmoteCooldown = new Date(); // the emote shown from using the !showemote <emote> command
-let showEmotePos = 1;
-let minStreak = 2; // minimum emote streak to trigger overlay effects
+let minStreak = getUrlParam('minStreak', 5);; // minimum emote streak to trigger overlay effects
 
 function findEmotes(message, messageFull) {
     if (emotes.length !== 0) {
@@ -133,6 +132,8 @@ function streakEvent() {
     if (currentStreak.streak >= minStreak) {
         $('#main').empty();
         $('#main').css("visibility","visible");
+        $('#main').css("position","absolute");
+        $('#main').css("top","650");
         var img = $('<img />', {src : currentStreak.emoteURL });
         img.appendTo('#main');
         var streakLength = $('#main').append(' x' + currentStreak.streak + ' streak!');
@@ -172,19 +173,46 @@ function showEmote(message, messageFull) {
 }
 
 function showEmoteEvent(emote) {
-    const cooldownTime = 10; // seconds
+    const cooldownTime = 5; // seconds
     let secondsDiff = (new Date().getTime() - new Date(showEmoteCooldown).getTime()) / 1000;
     log(secondsDiff)
     if (secondsDiff > cooldownTime) {
         showEmoteCooldown = new Date();
-        $(`#showEmote${showEmotePos}`).empty();
-        showEmotePos = Math.floor(Math.random() * 11);
-        $(`#showEmote${showEmotePos}`).css("visibility","visible");
-        var img = $('<img />', { src : emote.emoteURL });
-        img.appendTo(`#showEmote${showEmotePos}`);
-        setTimeout(() => {
-            $('#showEmote').empty();
-        }, 4 * 1000);
+        var canvas = document.querySelector('#mars');
+        var image = emote.emoteURL;
+        function getMeta(url, callback) {
+            var img = new Image();
+            img.src = url;
+            img.onload = function() { callback(this.width, this.height); }
+        }
+        getMeta(image, function(width, height) {
+            var max_height = 720;
+            var max_width = 1280;
+            function getRandomCoords() {
+                var r = [];
+                var x = Math.floor(Math.random() * max_width);
+                var y = Math.floor(Math.random() * max_height);
+            
+                r = [x, y];
+                return r;
+            };
+            function createImage() {
+                log(1)
+                var node = document.createElement('img');
+                var xy = getRandomCoords();
+            
+                node.id = "showEmote";
+                node.style.position = 'absolute';
+                node.style.top = xy[1] + 'px';
+                node.style.left = xy[0] + 'px';
+                node.setAttribute('src', image);
+                canvas.appendChild(node);
+                setTimeout(() => {
+                    $('#mars').empty();
+                }, 4 * 1000);
+            }
+            createImage();
+        });
     }
 }
 
@@ -220,6 +248,10 @@ function connect() {
             let message = (messageBefore.split(' ').includes('ACTION')) ? messageBefore.split('ACTION ').pop().split('')[0] : messageBefore; // checks for the /me ACTION usage and gets the specific message
             if (message.toLowerCase().startsWith('!showemote')) { showEmote(message, messageFull); }
             findEmotes(message, messageFull)
+        }
+        if (messageFull.length == 1 && messageFull[0].startsWith("PING")) {
+            log('sending pong')
+            chat.send("PONG")
         }
     };
 }
