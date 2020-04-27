56a7055ee46da46f901d3f1dc3d23a17769b8ec4
Jahaan Jain <jahaanjain@gmail.com>
2020-04-27 15:31:16 -0400

    Fixed some animations, & made minStreak default to at least 5

--- main.js
+++ main.js
@@ -93,8 +93,7 @@ async function getEmotes(check) {
 let currentStreak = { streak: 1, emote: null, emoteURL: null }; // the current emote streak being used in chat
 let currentEmote; // the current emote being used in chat
 let showEmoteCooldown = new Date(); // the emote shown from using the !showemote <emote> command
-let minStreak = (getUrlParam('minStreak', 5) > 1) ? getUrlParam('minStreak', 5) : 5; // minimum emote streak to trigger overlay effects
-let streakCD; // remove streak from overlay if it hasnt changed in a while
+let minStreak = (getUrlParam('minStreak', 5) > 4) ? getUrlParam('minStreak', 5) : 5; // minimum emote streak to trigger overlay effects
 
 function findEmotes(message, messageFull) {
     if (emotes.length !== 0) {
@@ -138,22 +137,13 @@ function streakEvent() {
         img.appendTo('#main');
         var streakLength = $('#main').append(' x' + currentStreak.streak + ' streak!');
         streakLength.appendTo('#main');
-        $('#main').css("visibility","visible");
         gsap.to("#main", 0.15, { scaleX: 1.2, scaleY: 1.2, onComplete: downscale });
-        function downscale() {
-            gsap.to("#main", 0.15, { scaleX: 1, scaleY: 1 });
-        }
-        streakCD = $('#main').html();
-        setTimeout(() => {
-            if (streakCD === $('#main').html()) {
-                gsap.to("#main", 0.2, { scaleX: 0, scaleY: 0, onComplete: vanish });
-            }
-            function vanish() {
-                log('forcing streak hide'); $('#main').css("visibility","hidden");
-            }
-        }, 5 * 1000);
+        function downscale() { gsap.to("#main", 0.15, { scaleX: 1, scaleY: 1 }); }
     }
-    if (currentStreak.streak < minStreak) { log('streak changed, now hiding..'); $('#main').css("visibility","hidden"); }
+    if (currentStreak.streak < minStreak && currentStreak.streak > 3) {
+        log('streak changed, now hiding..');
+        gsap.to("#main", 0.2, { scaleX: 0, scaleY: 0, delay: 2 });
+    }
 }
 
 function showEmote(message, messageFull) {
